88047f1427fa8da58fdd897de70ea01accede9e1
Warmist <warmist@gmail.com>
2019-08-28 17:07:34 +0300

    even better summon army

--- summon-army.lua
+++ summon-army.lua
@@ -7,13 +7,20 @@ function find_player_group(  )
 		end
 	end
 end
+
+local _,pa=find_player_group()
+if pa==nil then
+	qerror("player army not found")
+end
+local nr=df.nemesis_record.find(pa.members[0].nemesis_id)
+local pfig=nr.figure
+
 local army_contents={
-	--fill out this
 	count=15,
-	race=572,
-	civ_id=246,
-	population_id=229,
-	cultural_identity=937,
+	race=pfig.race,
+	civ_id=pfig.civ_id,
+	population_id=pfig.population_id,
+	cultural_identity=pfig.cultural_identity,
 
 	new=true,
 	--- unknown stuff
@@ -24,26 +31,23 @@ local army_contents={
 	unk_24=-1,
 	unk_28=-1,
 }
-local _,pa=find_player_group()
-if pa==nil then
-	qerror("player army not found")
-end
---df.global.army_next_id
+
 local armies=df.global.world.armies.all
-local army=df.army:new()
-army.id=df.global.army_next_id
+local army={
+	new=true,
+	id=df.global.army_next_id,
 --trying to "trick" game into moving army into player square
 --this should also work for migrants?
-army.pos={x=pa.pos.x-1,y=pa.pos.y,z=0}
-army.unk_pos_x:insert("#",pa.pos.x)
-army.unk_pos_y:insert("#",pa.pos.y)
-army.unk_2c:insert("#",army_contents)
+	pos={x=pa.pos.x-1,y=pa.pos.y,z=0},
+	unk_pos_x={pa.pos.x},
+	unk_pos_y={pa.pos.y},
+	unk_2c=army_contents,
 --who knows?
-army.unk_90=-1
-army.unk_94=-1
-army.unk_98=-1
-army.unk_9c=100
-army.anon_1=10000
-
+unk_90=-1,
+unk_94=-1,
+unk_98=-1,
+unk_9c=100,
+anon_1=10000,
+}
 armies:insert("#",army)
 df.global.army_next_id=df.global.army_next_id+1
